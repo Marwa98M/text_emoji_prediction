d67d7295902a3bba2d56db080fbc6c55d7cd255b
Marwa98M <mm1604391@gmail.com>
2024-04-25 02:25:21 +0300

    Adding a readme file

--- src/README.md
+++ src/README.md
@@ -1,1 +1,12 @@
-# text_emoji_prediction
+Text-to-Emoji Prediction with MistralAI
+
+Overview
+
+Installation
+
+
+Examples
+Input Text: "How are you feeling today?"
+Predicted Emojis: 😊👍🌞
+Input Text: "I had a great day at work!"
+Predicted Emojis: 😄🎉👏
--- src/main.py
+++ src/main.py
@@ -1,34 +1,46 @@
-import re
-from huggingface_hub import InferenceClient
-import random
-import time
-import gradio as gr
+# regular expression 
+import re 
+# huggingface_hub: a repository for pre-trained models for machine learning tasks.
+# InferenceClient: loading and utilizing models hosted on the Hugging Face Model Hub.
+from huggingface_hub import InferenceClient 
 
+
+# name of the model identifier
 client = InferenceClient("mistralai/Mixtral-8x7B-Instruct-v0.1")
 
 system_instructions = """<s> [INST] You will be provided with text, and your task is to translate it into emojis. DO NOT USE ANY REGULAR TEXT. Do your best with emojis only. Translate this text: """
 
 def generate_translation(prompt):
-    generate_kwargs = dict(
-        temperature=0.5,
-        max_new_tokens=1024,
-        top_p=0.95,
-        repetition_penalty=1.0,
-        do_sample=True,
-        seed=42,
+    # dictionary containing  parameters that control how the text is generated,
+    generate_kwargs = dict( # parameters for gpt
+        temperature=0.5, # Controls the randomness of the generated text
+        max_new_tokens=1024, # Limits the maximum number of tokens
+        top_p=0.95, # Tokens with cumulative probability above this threshold are filtered out
+        repetition_penalty=1.0, # Penalizes repeating tokens in the generated text
+        do_sample=True, # randomly selects the next token based on the model's probabilities.
+        seed=42, # same prompt --> same output
     )
 
     formatted_prompt = system_instructions + prompt + "[/INST]"
     stream = client.text_generation(
-        formatted_prompt, **generate_kwargs, stream=True, details=True, return_full_text=False)
+        formatted_prompt, 
+        **generate_kwargs, 
+        stream=True, # chunks rather than all at once
+        details=True, # additional details about the text generation
+        return_full_text=False) # If True, generated text will be returned as a single string, rather than streamed.
+    print(stream)
     
     output = ""
     for response in stream:
         output += response.token.text
 
+    
+    # Define emoji pattern
     emoji_pattern = r"[^\u0021-\u007E\u00A0-\uD7FF\uE000-\uFDCF\uFF00-\uFFEF\u10000-\u10FFFF\u0300-\u036F\u1F00-\u1F1F\u1F20-\u1F7F\u2600-\u26FF\u2700-\u27BF]+?"
 
+    # Find emojis and non-ASCII characters
     filtered_output = re.findall(emoji_pattern, output)
+
 
     return ''.join(filtered_output).replace(" ", "").replace("\n", "")
 
